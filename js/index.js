22a856d6aa20d2c8b605bcf54346709601c38dd2
waheedchitapure <wchitapure@gmail.com>
2021-07-03 14:48:23 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// gsap.from('content-right', { duration: 2, x: '-100%' });
+gsap.to('.content-right', {
+    duration: 1,
+    x: 200,
+    scale: 2
+})
+
+gsap.to('.new', {
+    duration: 1,
+    x: 200,
+    scale: 2
+})
